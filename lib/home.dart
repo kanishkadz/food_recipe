a1f5c3093044f3916b51925983ab69143113f84f
Kanishka Anand <kanishkaadz@gmail.com>
2024-10-17 22:21:15 +0530

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -9,6 +9,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   TextEditingController searchController = new TextEditingController();
+  String url = "https://api.edamam.com/search?q=$query&app_id=c90bf8d1&app_key=aca1f0d547799ccb805d117c94b040f0";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
