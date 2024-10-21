706da5f076e3ed15f9f5c2f35315d0774aebfa51
Kanishka Anand <kanishkaadz@gmail.com>
2024-10-21 21:57:48 +0530

    Update model.dart

--- lib/model.dart
+++ lib/model.dart
@@ -1,16 +1,22 @@
-class RecipeModel{
-  String applabel;
-  String appimgUrl;
+class RecipeModel {
+  String? applabel;
+  String? appimgUrl;
   double appcalories;
-  String appurl;
+  String? appurl;
 
-  RecipeModel({this.applabel,required this.appcalories, this.appimgUrl, this.appurl});
-  factory RecipeModel.fromMap(Map recipe){
+  RecipeModel({
+    this.applabel,
+    required this.appcalories,
+    this.appimgUrl,
+    this.appurl,
+  });
+
+  factory RecipeModel.fromMap(Map<String, dynamic> recipe) {
     return RecipeModel(
-      applabel: recipe["label"],
-      appcalories: recipe["calories"],
-      appimgUrl: recipe["image"],
-      appurl: recipe["url"]
+      applabel: recipe["label"] as String?,
+      appcalories: recipe["calories"] as double,
+      appimgUrl: recipe["image"] as String?,
+      appurl: recipe["url"] as String?,
     );
   }
 }
