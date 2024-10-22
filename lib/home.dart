7e410f401555c2bba4a2cc381c3470e011ce9367
Kanishka Anand <kanishkaadz@gmail.com>
2024-10-22 21:45:28 +0530

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -131,6 +131,7 @@ class _HomeState extends State<Home> {
                                 recipeList[index].appimgUrl
                                 fit: BoxFit.cover,
                               width: double.infinity,
+                              height: 200,
                             ),
                           ),
 
